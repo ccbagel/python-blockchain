9ed80cc9e3e0722ccd576f44504c64378632bab5
ccbagel <hasan.abdi.ahmed97@gmail.com>
2021-07-23 08:39:51 -0600

    complete

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
+
+import hashlib
+
+# initilize a class for the blockchain
+class CCbagelBlock:
+    # class initializer
+    def __init__(self, previous_block_hash, transaction_list):
+        self.previous_block_hash = previous_block_hash
+        self.transaction_list = transaction_list
+
+        # constructing data strings
+        self.block_data = " - ".join(transaction_list) + " - " + previous_block_hash
+
+        # calculate hash
+        self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+
+
+t1 = "Mike sends Anna 2 CC"
+t2 = "Bob sends Anna 4 CC"
+t3 = "Anna sends 4 CC to Oscar"
+t4 = "Oscar sends 30 CC to Jake"
+
+initial_block = CCbagelBlock("First block initialized", [t1, t2])
+
+print(initial_block.block_data)
+print(initial_block.block_hash)
+
+second_block = CCbagelBlock(initial_block.block_hash, [t3, t4])
+
+print(second_block.block_data)
+print(second_block.block_hash)
+
 '''
-This project is a simple blockchain built in Python. It uses SHA256 encryption and a simple hash function. 
-The users are objects generated via class, each object has an address & key which are used to send transactions. Names could be used for the general profile, but for privacy I'll emit them from the transaction.
+To test this out, you can download this project and run it in your terminal. Try changing the amount of CC sent in t1, t2, t3, t4 and compare the block hashes. 
 '''
